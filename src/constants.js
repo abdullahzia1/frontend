a138267ade69e8ca45e57416414f97af5cfbee58
Abdullah <abziawork960@gmail.com>
2025-05-05 02:16:36 +0500

    deployment

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 
 // export const BASE_URL = 'http://localhost:5000'; // If using proxy 
-export const BASE_URL = 'http://51.21.220.105:5000'; // Replace with your production URL
+export const BASE_URL = 'https://treasurelane-market.com/api'; // Replace with your production URL
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
